0a01bb7d95626c45ce593581fefe41c01608670d
waneka <twwaneka@gmail.com>
2015-12-08 15:22:08 -0800

    fixes copy error

--- cron-builder.js
+++ cron-builder.js
@@ -13,11 +13,11 @@ CronBuilder.prototype.build = function () {
     var expressionArray = [];
 
     expressionArray.push(this.expression.minute.join(','));
-    expressionArray.push(this.expression.minute.join(','));
-    expressionArray.push(this.expression.minute.join(','));
-    expressionArray.push(this.expression.minute.join(','));
-    expressionArray.push(this.expression.minute.join(','));
-    expressionArray.push(this.expression.minute.join(','));
+    expressionArray.push(this.expression.hour.join(','));
+    expressionArray.push(this.expression.dayOfTheMonth.join(','));
+    expressionArray.push(this.expression.monthOfTheYear.join(','));
+    expressionArray.push(this.expression.dayOfTheWeek.join(','));
+    expressionArray.push(this.expression.year.join(','));
 
     return expressionArray.join(' ');
 };
