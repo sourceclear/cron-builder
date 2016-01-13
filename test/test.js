b96b41b243ce684140f3da749e42c6e596b83d7d
waneka <twwaneka@gmail.com>
2016-01-12 18:32:57 -0800

    moving to a closure to make the expression "private"
    - refactoring to allow for such a restructure
    - fixing a broken test

--- test/test.js
+++ test/test.js
@@ -7,11 +7,11 @@ describe('cron-builder', function () {
 
     it('defaults to "* * * * *" when initialized without arguments', function () {
         cron = new cb();
-        expect(cron.expression.minute).to.eql(['*']);
-        expect(cron.expression.hour).to.eql(['*']);
-        expect(cron.expression.dayOfTheMonth).to.eql(['*']);
-        expect(cron.expression.month).to.eql(['*']);
-        expect(cron.expression.dayOfTheWeek).to.eql(['*']);
+        expect(cron.get('minute')).to.equal('*');
+        expect(cron.get('hour')).to.equal('*');
+        expect(cron.get('dayOfTheMonth')).to.equal('*');
+        expect(cron.get('month')).to.equal('*');
+        expect(cron.get('dayOfTheWeek')).to.equal('*');
     });
 
     it('returns a working cron expression when calling .build()', function () {
